8c84a7e9549c54b0e90ef0a31e4be04e074cac61
Kyera Vassell <143655706+KyeraV@users.noreply.github.com>
2023-09-18 16:35:55 -0400

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -1,5 +1,10 @@
 // Import libraries here
-/* Goal: 
+/* Goal: Print out your initials for your first and last name in large letter blocks.
+Next ask user to enter a number in Fahrenheit, the program should convert the value to
+Celsius, save it. take the input from the user in a 5-character string, save the string
+the string in reverse without the first and last characters. Next generate and save a 
+random number between 32 - 16,384. Finally combine the results from steps 2 - 4 and print
+ it to the screen.*/
 public class Assignment1 {
   public static void main(String[] args) {
     // Add your code here
