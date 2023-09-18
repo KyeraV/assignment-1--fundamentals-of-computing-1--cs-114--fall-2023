5cc13eae00d53bde706b063a98f4b3338f0b5075
Kyera Vassell <143655706+KyeraV@users.noreply.github.com>
2023-09-18 16:37:39 -0400

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -9,14 +9,14 @@ public class Assignment1 {
   public static void main(String[] args) {
     // Add your code here
     System.out.println();
-    System.out.println("          kkk        kkk     VVV               VVV);
-    System.out.println("          kkk      kkk        VVV             VVV);
-    System.out.println("          kkk     kkk          VVV           VVV);
-    System.out.println("          kkk   kkk             VVV         VVV);
-    System.out.println("          kk  kkk                VVV       VVV);
-    System.out.println("          kkk   kkk               VVV     VVV);
-    System.out.println("          kkk     kkk              VVV   VVV);
-    System.out.println("          kkk       kkk             VVV VVV);
-    System.out.println("          kkk         kkk             VVV);
+    System.out.println(          kkk        kkk     VVV               VVV);
+    System.out.println(          kkk      kkk        VVV             VVV);
+    System.out.println(          kkk     kkk          VVV           VVV);
+    System.out.println(          kkk   kkk             VVV         VVV);
+    System.out.println(          kk  kkk                VVV       VVV);
+    System.out.println(          kkk   kkk               VVV     VVV);
+    System.out.println(          kkk     kkk              VVV   VVV);
+    System.out.println(          kkk       kkk             VVV VVV);
+    System.out.println(          kkk         kkk             VVV);
   }
 }
