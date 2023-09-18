351ecc1f97ecdfe8f7ef054155d14e9c5108793b
Kyera Vassell <143655706+KyeraV@users.noreply.github.com>
2023-09-18 16:24:00 -0400

    Part 1 Initials

--- Assignment1.java
+++ Assignment1.java
@@ -1,7 +1,17 @@
 // Import libraries here
-
+/* Goal: 
 public class Assignment1 {
   public static void main(String[] args) {
     // Add your code here
+    System.out.println();
+    System.out.println("          kkk        kkk     VVV               VVV);
+    System.out.println("          kkk      kkk        VVV             VVV);
+    System.out.println("          kkk     kkk          VVV           VVV);
+    System.out.println("          kkk   kkk             VVV         VVV);
+    System.out.println("          kk  kkk                VVV       VVV);
+    System.out.println("          kkk   kkk               VVV     VVV);
+    System.out.println("          kkk     kkk              VVV   VVV);
+    System.out.println("          kkk       kkk             VVV VVV);
+    System.out.println("          kkk         kkk             VVV);
   }
 }
